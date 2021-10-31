37553dd10a0f0589c5c71779db04757db24f4616
MMartinHan <mmahan@espe.edu.ec>
2021-10-31 11:49:06 -0500

    Correcion

--- Grupo-2-Nrc-7167/Sarmiento Alejandro/Deberes/Domingo 31 Oct/c docs/html/search/functions_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['getden_13',['getDen',['../class_fraccion.html#acabf9001ec94686306dfcac637916545',1,'Fraccion']]],
-  ['getnum_14',['getNum',['../class_fraccion.html#ae0ad481be7f1023f4f58b2a0ba87ad82',1,'Fraccion']]]
-];
