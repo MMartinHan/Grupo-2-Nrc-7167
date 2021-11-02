3c9d2a12badeeff48235fe2077bf695040988c38
AlejandroSarmientoC <basarmiento@espe.edu.ec>
2021-11-02 08:32:43 -0500

    Deber Diario Martes 02 Nov

---
+++ Grupo-2-Nrc-7167/Sarmiento Alejandro/Deberes/Domingo 31 Oct/c docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['calcular_0',['calcular',['../class_fraccion.html#a2deda715ccc42c3ec45f296f5c8edb86',1,'Fraccion']]],
+  ['calculo_1',['Calculo',['../class_calculo.html',1,'']]]
+];
