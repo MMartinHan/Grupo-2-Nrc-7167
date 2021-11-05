b77b6082e409059a168389b421c8db3ee724ea16
josuealeman17 <eljossam@hotmail.com>
2021-11-04 23:35:49 -0500

    Removiendo archivos no prescindibles

--- PARCIAL I/Deber 2. Templates/Sarmiento Alejandro/Deberes/Domingo 31 Oct/c docs/html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['calcular_0',['calcular',['../class_fraccion.html#a2deda715ccc42c3ec45f296f5c8edb86',1,'Fraccion']]],
-  ['calculo_1',['Calculo',['../class_calculo.html',1,'']]]
-];
--- PARCIAL I/Deber 2. Templates/Sarmiento Alejandro/Deberes/Domingo 31 Oct/c docs/html/search/functions_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pedirdatos_16',['pedirDatos',['../class_fraccion.html#a94441e25add273374207dd8e43865c5b',1,'Fraccion']]]
-];
