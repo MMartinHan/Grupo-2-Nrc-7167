bb97aff0d6302939b4f062d726c1d3741298480a
Martin Han <mmhan@espe.edu.ec>
2021-11-04 22:47:03 -0500

    Actualizacion

---
+++ Grupo-2-Nrc-7167/Han Martin/Plantilla 3/UnitTestEjercicio/UnitTestEjercicioViernes29/UnitTestEjercicioViernes29.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestEjercicioViernes29
+{
+	TEST_CLASS(UnitTestEjercicioViernes29)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ Grupo-2-Nrc-7167/Han Martin/Plantilla 7/c docs/html/search/functions_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pedirdatos_16',['pedirDatos',['../class_fraccion.html#a94441e25add273374207dd8e43865c5b',1,'Fraccion']]]
+];
